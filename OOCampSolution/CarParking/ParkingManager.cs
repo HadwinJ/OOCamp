a9cfbc1f09d3900fa6e46e8bbe96cd39efdbee4d
Hadwin Jiang <hadwinjiang@126.com>
2016-12-26 13:58:51 +0800

    Added Parking Manager

--- OOCampSolution/CarParking/ParkingManager.cs
+++ OOCampSolution/CarParking/ParkingManager.cs
@@ -8,5 +8,28 @@ namespace CarParking
 {
     public class ParkingManager
     {
+        private List<ParkingBoy> _parkingBoyList = new List<ParkingBoy>();
+
+        public int AvailableNumber
+        {
+            get { return _parkingBoyList.Sum(pb => pb.AvailableNumber); }
+        }
+
+        public void AddParkingBoy(ParkingBoy myParkingBoy)
+        {
+            _parkingBoyList.Add(myParkingBoy);
+        }
+
+        public Tuple<string, int> Park(Car myCar)
+        {
+            var parkingBoy = _parkingBoyList.OrderByDescending(p => p.AvailableNumber).FirstOrDefault();
+            return parkingBoy?.Park(myCar);
+        }
+
+        public Car Pick(Tuple<string, int> parkingId)
+        {
+            var parkingBoy = _parkingBoyList.Where(p => p.ParkingStations.Any(ps => ps.Name == parkingId.Item1)).FirstOrDefault();
+            return parkingBoy.Pick(parkingId);
+        }
     }
 }
---
+++ OOCampSolution/CarParkingTests/ParkingManagerTests.cs
@@ -0,0 +1,77 @@
+﻿using Xunit;
+using CarParking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarParking.Tests
+{
+    public class ParkingManagerTests
+    {
+        [Fact()]
+        public void ParkingBoyTest()
+        {
+            var parkingBoy = new ParkingBoy();
+            Assert.True(true);
+        }
+
+        [Fact()]
+        public void ParkingAndPickingBack()
+        {
+            var myCar = new Car();
+            var myParkingBoy = new ParkingBoy(new List<ParkingStation> {
+                new ParkingStation("Station1"),
+                new ParkingStation("Station2")
+            });
+            var myParkingManager = new ParkingManager();
+
+            myParkingManager.AddParkingBoy(myParkingBoy);
+            var parkingId = myParkingManager.Park(myCar);
+
+            var myReceiving = myParkingManager.Pick(parkingId);
+
+            Assert.Equal<Car>(myCar, myReceiving);
+        }
+
+        [Fact()]
+        public void ShowInitialAvailableSpace()
+        {
+            // given
+            var myParkingBoy = new ParkingBoy(new List<ParkingStation> {
+                new ParkingStation("Station1"),
+                new ParkingStation("Station2")
+            });
+            var myParkingManager = new ParkingManager();
+            myParkingManager.AddParkingBoy(myParkingBoy);
+
+            // when
+
+            // then
+            Assert.Equal(20, myParkingManager.AvailableNumber);
+        }
+
+        [Fact()]
+        public void ShowAvailableSpace()
+        {
+            // given
+            var myCar = new Car();
+            var myParkingBoy = new ParkingBoy(new List<ParkingStation> {
+                new ParkingStation("Station1"),
+                new ParkingStation("Station2")
+            });
+            var myParkingManager = new ParkingManager();
+            myParkingManager.AddParkingBoy(myParkingBoy);
+
+            // when
+            var parkingId = myParkingBoy.Park(myCar);
+
+            // then
+            Assert.Equal(19, myParkingManager.AvailableNumber);
+        }
+        
+        
+
+    }
+}
